de2cb0f81ef3164987aad548ca4673d6a199db13
Prathicksha0504 <vp9403@srmist.edu.in>
2023-12-13 19:57:25 +0530

    tried a bit for dash board screen but tis not good

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -2,62 +2,190 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
 
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Finance Dashboard',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DashboardScreen(),
+    );
+  }
+}
 
 class DashboardScreen extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Collection Dashboard'),
+        title: Text('Finance Dashboard'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Transactions'),
+              onTap: () {
+                // Add navigation to transactions page
+              },
+            ),
+            // Add more items for different sections
+          ],
+        ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Daily Collection: \$500', // Replace with your actual daily collection data
-              style: TextStyle(fontSize: 18),
+              'Account Overview',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            SizedBox(height: 16),
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Balance',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      '\$10,000.00',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildSummaryCard('Income', '\$5,000.00', Colors.green),
+                _buildSummaryCard('Expenses', '\$3,000.00', Colors.red),
+              ],
+            ),
+            SizedBox(height: 16),
             Text(
-              'Total Collection: \$5000', // Replace with your actual total collection data
-              style: TextStyle(fontSize: 18),
+              'Summary Chart',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            SizedBox(height: 8),
+            AspectRatio(
+              aspectRatio: 1.3,
+              child: Card(
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: PieChart(
+                    PieChartData(
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 40,
+                      sections: [
+                        PieChartSectionData(
+                          color: Colors.green,
+                          value: 5000,
+                          title: 'Income',
+                          radius: 50,
+                          titleStyle: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xffffffff),
+                          ),
+                        ),
+                        PieChartSectionData(
+                          color: Colors.red,
+                          value: 3000,
+                          title: 'Expenses',
+                          radius: 50,
+                          titleStyle: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: const Color(0xffffffff),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
             Text(
-              'Borrowers Count: 20', // Replace with your actual borrowers count data
-              style: TextStyle(fontSize: 18),
+              'Recent Transactions',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 8),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Transaction $index'),
+                    subtitle: Text('\$500.00'),
+                    trailing: Icon(Icons.arrow_forward),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSummaryCard(String title, String value, Color color) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Text(
-              'Collection Percentage:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            SizedBox(height: 10),
-            PieChart(
-              PieChartData(
-                sections: [
-                  PieChartSectionData(
-                    color: Colors.blue,
-                    value: 30, // Replace with your actual data
-                    title: '30%',
-                  ),
-                  PieChartSectionData(
-                    color: Colors.green,
-                    value: 40, // Replace with your actual data
-                    title: '40%',
-                  ),
-                  PieChartSectionData(
-                    color: Colors.orange,
-                    value: 30, // Replace with your actual data
-                    title: '30%',
-                  ),
-                ],
-                borderData: FlBorderData(show: false),
-                centerSpaceRadius: 40,
-                sectionsSpace: 0,
-                pieTouchData: PieTouchData(touchCallback: (FlTouchEvent event, touchController) {}),
+            SizedBox(height: 8),
+            Text(
+              value,
+              style: TextStyle(
+                fontSize: 20,
+                color: color,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ],
